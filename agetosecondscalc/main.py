1139ee69335ca3e9ab9421c2f51bea58e132a69a
Sebastian Saez <sebalon@gmail.com>
2018-01-08 22:19:48 -0300

    fixed return in agetoseoconds

--- agetosecondscalc/main.py
+++ agetosecondscalc/main.py
@@ -1,6 +1,7 @@
 def age_in_seconds(age):
-	return age * 365 * 24 * 60 * 60
+    return int(age) * 365 * 24 * 60 * 60
+
 
 user_age = input("Enter your age: ")
 
-print("Your age is {} and that equals to {} seconds".format(user_age, age_in_seconds(user_age)))
+print("Ok so your age is {} and that equals to {} seconds".format(user_age, age_in_seconds(user_age)))
